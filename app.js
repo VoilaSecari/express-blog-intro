e0f3a9cb4f93a1905d2fb4c1f70a9a6546b3d00e
VoilaSecari <lifeaslaurafilip@gmail.com>
2025-05-12 22:09:44 +0200

    immagini aggiunte e richiamo server

--- app.js
+++ app.js
@@ -13,3 +13,47 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.json({ message: "Ciao, questo è il mio blog!" });
 });
+
+// url per i post nella bacheca
+
+app.get("/bacheca", (req, res) => {
+  const posts = [
+    {
+      name: "Ciambellone",
+      description: "un ciambellone",
+      img: "/img/ciambellone.jpg",
+      tags: "ciambella",
+    },
+    {
+      name: "Cracker alla Barbabietola",
+      description: "Dei crackers alla barbabietola",
+      img: "/img/cracker_barbabietola.jpg",
+      tags: "cracker, barbabietola",
+    },
+    {
+      name: "Pane Fritto Dolce",
+      description: "Del pane fritto dolce",
+      img: "/img/pane_fritto_dolce.jpg",
+      tags: "pane, fritto, dolce",
+    },
+    {
+      name: "Pasta alle Barbabietole",
+      description: "Della pasta alla barbabietola",
+      img: "/img/pasta_barbabietola.jpg",
+      tags: "pasta, barbabietola",
+    },
+    {
+      name: "Torta Paesana",
+      description: "Della Torta paesana",
+      img: "/img/torta_paesana.jpg",
+      tags: "torta, paesana",
+    },
+  ];
+  res.json(posts);
+});
+
+// ascolto
+
+app.listen(port, () => {
+  console.log("Server in ascolto alla porta " + port);
+});
